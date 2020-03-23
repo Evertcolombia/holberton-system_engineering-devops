3781b5d55eaae2adb5b15eb8003e6a21d6a1f1fa
evertcolombia <evertescalante@gmail.com>
2020-03-23 19:04:56 +0000

    hotfix file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
 # Create a file  in path /tmp/holberton
 
 file { 'newc file /tmp/holberton':
-    path: => '/tmp/holberton',
-    ensure: => 'present',
-    mode: => 'u +rwx',
-    owner: => 'www-data',
-    group: => 'www-data',
-    content: => 'I love Puppet'
+    ensure  => 'present',
+    path    => '/tmp/holberton',
+    mode    => 'u+rwx',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet'
 }
