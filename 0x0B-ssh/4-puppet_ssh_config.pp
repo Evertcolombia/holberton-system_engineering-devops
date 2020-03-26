8e3c31951d715b48769838822cabdeba5204c245
evertcolombia <evertescalante@gmail.com>
2020-03-26 02:36:56 +0000

    hotfix

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,9 +1,9 @@
 # manifies to config file in sever with puppet
 
 file_line { 'password authentication' :
-  ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
-  line   => 'PasswordAuthentication no',
+    ensure => 'present',
+    path   => '/etc/ssh/ssh_config',
+    line   => 'PasswordAuthentication no',
 }
 
 file_line { 'look for file':
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 #this repo contains some scripts executables on bash
+0x09-web_infrastructure_design
