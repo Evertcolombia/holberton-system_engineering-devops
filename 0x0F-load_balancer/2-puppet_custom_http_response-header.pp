d1a144d472861dccc40e9c44b4114fc998d4f839
evertcolombia <evertescalante@gmail.com>
2020-04-09 02:01:35 +0000

    puppet response header config

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -0,0 +1,28 @@
+# update headers response to nginx
+
+exec { 'apt-get':
+    path    => '/usr/bin',
+    command => '/usr/bin/apt-get update',
+}
+
+package { 'nginx':
+    ensure  => installed
+}
+
+exec { 'mkdir-directory':
+    path    => '/bin',
+    command => '/bin/mkdir -p /var/www/html',
+    require => Package['nginx']
+}
+
+file { '/var/www/html/index.html':
+    ensure  => file,
+    path    => '/var/www/html/index.html',
+    content => 'Holberton School',
+    require => Exec['mkdir-directory'],
+}
+
+exec { 'header_response':
+    command => '/bin/sed -i "60i\ \tadd_header X-Served-By $HOSTNAME;" /etc/nginx/nginx.conf',
+    require => Package['nginx']
+}
