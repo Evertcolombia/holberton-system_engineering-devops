02494602b3612d5a0d393b8d9122d942774f7656
evertcolombia <evertescalante@gmail.com>
2020-04-09 02:16:33 +0000

    hotfix

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,7 +1,6 @@
 # update headers response to nginx
 
 exec { 'apt-get':
-    path    => '/usr/bin',
     command => '/usr/bin/apt-get update',
 }
 
@@ -15,21 +14,24 @@ service { 'nginx':
 }
 
 exec { 'mkdir-directory':
-    path    => '/bin',
     command => '/bin/mkdir -p /var/www/html',
     require => Package['nginx']
 }
 
 file { '/var/www/html/index.html':
     ensure  => file,
-    path    => '/var/www/html/index.html',
     content => 'Holberton School',
     require => Exec['mkdir-directory'],
 }
 
+exec { 'redirect':
+  command => '/bin/sed -i \'s|_;|_;\n\trewrite ^/redirect_me/$ https://www.youtube.com/watch?v=QH2-TGlwu4 permanent;|g\' /etc/nginx/sites-available/default',
+  require => Package['nginx']
+}
+
 exec { 'header_response':
-    command => '/bin/sed -i "60i\ \tadd_header X-Served-By $HOSTNAME;" /etc/nginx/nginx.conf',
-    require => Package['nginx']
+    command => '/bin/sed -i \'s|_;|_;\n\tadd_header \'X-Served-By\' \"\$HOSTNAME\"; |g\' /etc/nginx/sites-available/default',
+    require => Exec['redirect']
 }
 
 exec { 'restart':
