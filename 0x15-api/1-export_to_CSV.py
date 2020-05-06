836dbd6dce801537a3b0c5f857b2dd98bfa2868b
evertcolombia <evertescalante@gmail.com>
2020-05-06 17:42:43 -0500

    hotfix

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -2,9 +2,8 @@
 """
 get data from an api based on the userd id that is passed as argument
 """
-
-import requests
 import csv
+import requests
 from sys import argv
 
 
@@ -33,5 +32,5 @@ if __name__ == "__main__":
             for obj in todos_obj:
                 line = [obj['userId'], name, obj['completed'], obj['title']]
                 writer = csv.writer(f, delimiter=',', quotechar='"',
-                                    quoting=csv.QUOTE_ALL)
+                                   quoting=csv.QUOTE_ALL)
                 writer.writerow(line)
