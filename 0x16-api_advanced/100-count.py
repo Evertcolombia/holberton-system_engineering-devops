e6d5b3e61d206d2ae95d0bc89d8651fc25ceb31a
evertcolombia <evertescalante@gmail.com>
2020-05-07 23:09:25 -0500

    hotfix

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
+import operator
 import requests
 
 def do_request(subreddit, after):
     """ Do request to endopoint"""
-    url = 'https://www.reddit.com/r/{}/hot.json?limit=50&after={}'
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=100&after={}'
     userA = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:76.0)\
             Gecko/20100101 Firefox/76.0"
 
@@ -17,7 +18,6 @@ def recurse(subreddit, hot_list=[], after=''):
     """ do recursion while there are a next page of
         posts
     """
-    print("pass")
     res = do_request(subreddit, after)
     if res.status_code != 200:
         return None
@@ -52,7 +52,8 @@ def count_words(subreddit, word_list, i=0, hot_list=[], res={}):
     if i != len(word_list):
         count_words(subreddit, word_list, i, hot_list, res)
     else:
-        print(res)
-    """if after is None:
-        return None
-    return count_words(subreddit, word_list, after, c, i)"""
+        sort_res = {}
+        sort_res = dict(sorted(res.items(), key=operator.itemgetter(1), reverse=True))
+        for k,v in  sort_res.items():
+            if v > 0:
+                print('{}: {}'.format(k, v))
